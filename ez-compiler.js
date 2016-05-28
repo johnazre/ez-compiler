f8ebfa3f72b778b5536390f401d112883d5f91ed
John A <simplre1@gmail.com>
2016-05-27 21:51:51 -0700

    added babel, but need to figure out how to add babel script to npm output

--- ez-compiler.js
+++ ez-compiler.js
@@ -8,9 +8,12 @@ const exec = require('child_process').exec;
 // argv = ['node', 'ez-compiler', task]
 let task = process.argv[2];
 
+console.log(FS.readdirSync('./'))
+
 // read ez.complr
 FS.readFile('ez.complr', 'utf8', (error, data) => {
-	error ? console.log('INVALID FILE') : true;
+	// console.log(arguments)
+	error ? console.log(error) : true;
 
 	let command = '';
 	
@@ -25,25 +28,28 @@ FS.readFile('ez.complr', 'utf8', (error, data) => {
 			return bits;
 		})(task, data);
 		
+		console.log('datas: ', data);
 		const reqMods = commandParts[0].slice(9).split(',')
 		const source = commandParts[1].slice(8);
 		const destination = commandParts[2].slice(13)
 		
 		let commandCreator = reqMods.forEach((val) => {
 			if (val === "babel") {
-				command = command.concat(val + ' ' + source + ' -out-file ' + destination + " && ")
+				command = command.concat("npm run babel && ")
 			} else if (val === "coffee") {
 				command = command.concat(val + ' --out ' + destination + ' -c ' +  source + " && ")
+			} else if (val === "stylus") {
+				command = command.concat(val + ' ' + source + '/css --out ' + destination +  "/css && ")
 			} else {
 				command = command.concat(val + ' ' + source + ' -o ' + destination + " && ")
 			}
 
 		})
 		command = command.slice(0, -3);
-		fsExtra.copy('./src/test', './dist/test', function (err) {
-			if (err) return console.error(err)
-			console.log("success!")
-		})
+		// fsExtra.copy('./src', './dist', function (err) {
+		// 	if (err) return console.error(err)
+		// 	console.log("success!")
+		// })
 	} // end compile task
 	
 	// Wants to lint the file with Jshint
