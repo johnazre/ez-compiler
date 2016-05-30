f3ceaf43981ec96fbc30177fbb905e82bbfad829
johnazre <jaboyak@gmail.com>
2016-05-30 16:47:48 -0700

    refactored json to objects to make easier to manipulate

--- ez-compiler.js
+++ ez-compiler.js
@@ -4,8 +4,7 @@
 const FS = require('fs'),
 	  fsExtra = require('fs-extra'),
 	  exec = require('child_process').exec,
-	  createDir = require('./lib/create/createDir'),
-	  createFiles = require('./lib/create/createFiles');
+	  createDir = require('./lib/createDir');
 
 let command = 'concurrently ';
 
@@ -27,44 +26,31 @@ else if (task === "lint"){
 	command += ("jshint " + fileNameOrPath);
 }
 
-// Wants to lint the file with Jshint
+// Wants to test files with Mocha/Chai
 else if (task === "test"){
 	let fileNameOrPath = process.argv[3]
 	command += ("mocha " + fileNameOrPath);
 }
 
 else if (task === 'compile'){
-	// read ez.complr
-	FS.readFile('ez.complr', 'utf8', (error, data) => {
+
+	FS.readFile('ezconfig.json', 'utf8', (error, data) => {
 		error ? console.log(error) : true;
 		
-		FS.readFile('package.json', 'utf8', (error, data) => {
-			var scripts = require('./lib/scripts');
-			var result = data.replace(scripts.originalScript, scripts.nextScript);
-			FS.writeFile('./package.json', result, (err) => {
-				err ? console.log(err) : true;
-			});
-		})
-			
-		let commandParts = (() => {
-		let bits = data.toLowerCase()
-			.replace(/\s+/g, '')
-			.split("]");
-			return bits;
-		})(task, data);
+		let config = JSON.parse(data);
+		console.log(config);			
 		
-		console.log('datas: ', data);
+		const reqMods = config.preprocessors; // Required modules to use while creating a compile command
+		const source = config.source; // Where the file is
+		const destination = config.destination; // Where it's going
 		
-		const reqMods = commandParts[0].slice(9).split(',') // Required modules to use while creating a compile command
-		const source = commandParts[1].slice(8); // Where the file is
-		const destination = commandParts[2].slice(13) // Where it's going
 		
+		// Adds npm run command to each preprocessor
 		let commandCreator = reqMods.forEach((val) => {
-			command = command.concat("\"npm run " + val + "\" ") // Okay for now. Need to figure out better option
+			command = command.concat("\"npm run " + val + "\" ")
 		})
-		// command = command.slice(0, -2); // Trim the end of the last command.concat to remove "&& ".
 		
-		// What creates and executes the command
+		// What executes the command
 		console.log("command: ", command);
 		const child = exec(command, (error, stdout, stderr) => {
 			console.log("stdout: ", stdout);
